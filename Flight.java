0c221deb7e27c905603736fd5159caa1c259071f
hawkatiel <rachel.catdog@gmail.com>
2017-02-07 11:22:06 -0500

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,9 +1,18 @@
 public class Flight{
+  // my name kawan i am big weeb!
   private String flightNumber;
   private String departName;
   private String arrivalName;
   private int flightStatus; //0 = available, 1 = checking in, 2 = closed
   private double profit;
+  
+  public Flight(){
+    setFlightNumber(null);
+    setDepartName(null);
+    setArrivalName(null);
+    setFlightStatus(0);
+    setProfit(0);
+  }
 
   public Flight(String flightNumber, String departName, String arrivalName, int flightStatus, int double profit){
     setFlightNumber(flightNumber);
@@ -13,12 +22,12 @@ public class Flight{
     setProfit(profit);
   }
 
-  public String getNumber(){
-    return this.number;
+  public String getflightNumber(){
+    return this.flightNumber;
   }
 
-  public void setNumber(String number){
-    this.number = number;
+  public void setFlightNumber(String number){
+    this.flightNumber = number;
   }
 
   public String getDepartName(){
