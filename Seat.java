0d8fa7e67646fbb6533093e373713b5bc6d5d4eb
koowan <kwchan93@gmail.com>
2017-02-10 16:28:57 +0000

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -53,19 +53,18 @@ public class Seat{
  public void convertSeatNum(){
   seatNumber = this.seatNumber;
 
-  setSeatX(Integer.parseInt(seatNum.charAt(0)));
+  setSeatX(Integer.parseInt(seatNumber.charAt(0)));
 
-    if(seatNum.charAt(1) == "D"){
-    setSeatY(0);
-    }
-    else if(seatNum.charAt(1) == "C"){
-    setSeatY(1);
-    }
-    else if(seatNum.charAt(1) == "B"){
-    setSeatY(2);
-    }
-    else if(seatNum.charAt(1) == "A"){
-    setSeatY(3);
-    }
+  if(seatNumber.charAt(1).equals("D")){
+  setSeatY(0);
+  }
+  else if(seatNumber.charAt(1).equals("C")){
+  setSeatY(1);
+  }
+  else if(seatNumber.charAt(1).equals("B")){
+  setSeatY(2);
+  }
+  else if(seatNumber.charAt(1).equals("A")){
+  setSeatY(3);
   }
  }
