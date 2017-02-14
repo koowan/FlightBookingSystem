71a42b8ac09a4770677aed6d5affa824334c40a6
koowan <kwchan93@gmail.com>
2017-02-14 23:12:59 +0000

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,30 +1,3 @@
-public class Flight{
-  private String flightNumber;
-  private String departName;
-  private String arrivalName;
-  private int flightStatus; //0 = available, 1 = checking in, 2 = closed
-  private double profit;
-  
-  public Flight(){
-    setFlightNumber(null);
-    setDepartName(null);
-    setArrivalName(null);
-    setFlightStatus(0);
-    setProfit(0);
-  }
-
-  public Flight(String flightNumber, String departName, String arrivalName, int flightStatus, int double profit){
-    setFlightNumber(flightNumber);
-    setDepartName(departName);
-    setArrivalName(arrivalName);
-    setFlightStatus(flightStatus);
-    setProfit(profit);
-  }
-
-  public String getFlightNumber(){
-    return this.flightNumber;
-  }
-
 import java.util.*;
 
 public class Flight{
