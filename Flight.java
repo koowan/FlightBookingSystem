0d0ea8cd1ace5f0c46d21fe099d9e21113484355
hawkatiel <rachel.catdog@gmail.com>
2017-02-14 22:51:24 -0500

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,73 +1,102 @@
 import java.util.*;
 
 public class Flight{
-  private String flightNumber;
-  private String departName;
-  private String arrivalName;
-  private int flightStatus;
-  SeatStatus seatStatus;
-  Passenger passenger = new Passenger();
+	private String flightNumber;
+	private String departName;
+	private String arrivalName;
+	FlightStatus flightStatus;
+	Seat[][] seating = new Seat[4][8];
 
-  public Flight(){
-    setFlightNumber(null);
-    setDepartName(null);
-    setArrivalName(null);
-    setFlightStatus(0);
-  }
+	public static void main(String[] args) throws InputValidationException {
+		Flight test = new Flight();
+	}
 
-  public Flight(String flightNumber, String departName, String arrivalName, int flightStatus){
-    setFlightNumber(flightNumber);
-    setDepartName(departName);
-    setArrivalName(arrivalName);
-    setFlightStatus(flightStatus);
-  }
+	public Flight(){
+		setFlightNumber(null);
+		setDepartName(null);
+		setArrivalName(null);
+		setFlightStatus(FlightStatus.OPEN);
+		generateSeats();
+	}
+	
+	
+	public Flight(String flightNumber, String departName, String arrivalName, FlightStatus flightstatus){
+		setFlightNumber(flightNumber);
+		setDepartName(departName);
+		setArrivalName(arrivalName);
+		setFlightStatus(flightStatus);
+		generateSeats();
+	}
 
 
-  public String getFlightNumber(){
-    return this.flightNumber;
-  }
+	public String getFlightNumber(){
+		return this.flightNumber;
+	}
 
-  public void setFlightNumber(String number){
-    this.flightNumber = number;
-  }
+	public void setFlightNumber(String number){
+		this.flightNumber = number;
+	}
 
-  public String getDepartName(){
-    return this.departName;
-  }
+	public String getDepartName(){
+		return this.departName;
+	}
 
-  public void setDepartName(String departName){
-    this.departName = departName;
-  }
+	public void setDepartName(String departName){
+		this.departName = departName;
+	}
 
-  public String getArrivalName(){
-    return this.arrivalName;
-  }
+	public String getArrivalName(){
+		return this.arrivalName;
+	}
 
-  public void setArrivalName(String arrivalName){
-    this.arrivalName = arrivalName;
-  }
+	public void setArrivalName(String arrivalName){
+		this.arrivalName = arrivalName;
+	}
+	
+	public FlightStatus getFlightStatus(){
+		return this.flightStatus;
+	}
 
-  public int getFlightStatus(){
-    return this.flightStatus;
-  }
+	public void setFlightStatus(FlightStatus flightStatus){
+		this.flightStatus = flightStatus;
+	}
 
-  public void setFlightStatus(int flightStatus){
-    this.flightStatus = flightStatus;
-  }
+	
+	public void flightStatusDisplay(){
+		if (getFlightStatus() == FlightStatus.OPEN){
+			System.out.println("Available for booking");
+		} else if (getFlightStatus() == FlightStatus.CHECKING_IN){
+			System.out.println("Checking in");
+		} else if (getFlightStatus() == FlightStatus.CLOSED){
+			System.out.println("Closed");
+		}
+	}
 
-  public void flightStatusDisplay(){
-    if (getFlightStatus() == 0){
-      System.out.println("Available for booking");
-    }
-    else if (getFlightStatus() == 1){
-      System.out.println("Checking in");
-    }
-    else if (getFlightStatus() == 2){
-      System.out.println("Closed");
-    }
-  }
+	
+	public void generateSeats(){
+		// generates 4 row, 8 column grid of seats
+		for (int i = 0; i < 4; i ++) {
+			for (int j = 0; j < 8; j ++) {
+				this.seating[i][j] = new Seat(i,j);
+			}
+		}
+	}
 
-  public void generateSeats(){
-
-  }
-  }
+	/* make reservation
+	 * 
+	 * 
+	 */
+	
+	/* confirm reservation
+	 * 
+	 */
+	
+	/* cancel reservation
+	 * 
+	 */
+	
+	public void closeFlight(){
+		setFlightStatus(FlightStatus.CLOSED);
+	}
+	
+}
