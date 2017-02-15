7d2a9d106603d088879077a00d71b292907a69b0
koowan <kwchan93@gmail.com>
2017-02-15 01:21:41 +0000

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -4,8 +4,11 @@ public class Passenger{
     private String address;
     private String passportNum;
 
-  public Passenger(){
-
+  public Passenger(String name, String type, String address, String passportNum){
+    setName(name);
+    setType(type);
+    setAddress(address);
+    setPassportNum(passportNum);
     }
 
     public String getName(){
