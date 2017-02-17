14e768bc8f3bc50c086ef69199baa68904bc44a7
koowan <kwchan93@gmail.com>
2017-02-17 02:23:22 +0000

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class MainMenu{
   public static void main(String[] args) throws InputValidationException {
+    //set "PATH=%PATH%;C:\Program Files\Java\jdk1.8.0_60\bin"
     Scanner input = new Scanner(System.in);
     UserInterface userInterface = new UserInterface();
     FlightStatus flightStatus;
     SeatStatus seatStatus;
-    String userChoice = "";
-    char choice = '0';
     int switch1 = 0;
     int switch2 = 0;
 
@@ -15,21 +14,59 @@ public class MainMenu{
     Flight two = new Flight("4321", "Western Isle", "Glasgow", FlightStatus.CLOSED);
 
     try{
+      do{
         userInterface.displayMainMenu();
-        userChoice = input.next();
+        switch1 = input.nextInt();
         switch(switch1){
           case 1:
           do{
-            userInterface.displayAdminMenu();
-            userChoice = input.next();
+            System.out.println("Login required:")
+            System.out.println("---------------------------");
+            Staff staffaccount = new Staff("kawanc@scotairlines.com", "asdfghjkl");
+            StaffAccounts staffaccounts = new StaffAccounts();
+            staffaccounts.register(staffaccount);
+
+            System.out.println("Please enter your email:");
+            String email = input.nextLine();
+            input.nextLine();
+
+
+            System.out.println("Please enter your password:");
+            String password = input.nextLine();
+
+
+            Staff signIn = new Staff(email, password);
+            Cipher cipher = new RailFenceCipher();
+
+            if(cipher.verify(password, staffaccounts.lookup(email).getHashedPassword())){
+              System.out.println("Login Successful");
+              userInterface.displayAdminMenu();
+              switch2 = input.nextInt();
+            }
+            else{
+              System.out.println("Incorrect Email/Password");
+              return;
+            }
             switch(switch2){
               case 1:
-                System.out.print("weeb");
+                userInterface.displayFlightOneDetails();
+                userInterface.displayFlightTwoDetails();
+                System.out.println("Choose a flight to close:");
+                int closeFlight = input.nextInt();
+                if (closeFlight == 1){
+                  one.closeFlight();
+                  System.out.println("Flight one is now closed.")
+                  return;
+                }
+                else if(closeFlight == 2){
+                  two.closeFlight();
+                  System.out.println("Flight two is now closed.")
+                  return;
+                }
 
                 break;
             }//end switch 2
-            choice = userChoice.charAt(0);
-          } while (choice!='Q');
+          } while (switch2!=2);
             break;
           case 2:
             userInterface.displayFlightOneDetails();
@@ -50,6 +87,7 @@ public class MainMenu{
 
               System.out.println("Enter a seat number you wish to reserve/book:");
               String seatNum = input.nextLine();
+              input.nextLine();
               System.out.println("Enter your name:");
               String passengerName = input.nextLine();
               System.out.println("Are you a resident of the Western Isles? Y/N");
@@ -62,11 +100,11 @@ public class MainMenu{
               }
               System.out.println("Enter your address:");
               String address = input.nextLine();
+              input.nextLine();
               System.out.println("Enter your passport number:");
               String passportNum = input.nextLine();
               System.out.println("Enter company name:       (enter none if not business class)");
               String businessName = input.nextLine();
-
               one.makeReservation(seatNum, passengerName, resident, address, passportNum, businessName);
 
               System.out.println("");
@@ -90,6 +128,7 @@ public class MainMenu{
 
               System.out.println("Enter a seat number you wish to reserve/book:");
               String seatNum = input.nextLine();
+              input.nextLine();
               System.out.println("Enter your name:");
               String passengerName = input.nextLine();
               System.out.println("Are you a resident of the Western Isles? Y/N");
@@ -102,6 +141,7 @@ public class MainMenu{
               }
               System.out.println("Enter your address:");
               String address = input.nextLine();
+              input.nextLine();
               System.out.println("Enter your passport number:");
               String passportNum = input.nextLine();
               System.out.println("Enter company name:       (enter none if not business class)");
@@ -151,6 +191,7 @@ public class MainMenu{
               userInterface.displayFlightTwoDetails();
               break;
             }//end switch 1
+          } while (switch1!=5);
     }
     catch (InputValidationException e) {
       e.printStackTrace();
